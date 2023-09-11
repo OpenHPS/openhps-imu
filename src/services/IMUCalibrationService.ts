6cfc2dae8e1716b75a72b39bb481be3d82548591
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-09-11 11:12:05 +0200

    wip: calibration

--- src/services/IMUCalibrationService.ts
+++ src/services/IMUCalibrationService.ts
@@ -1,29 +1,90 @@
-import { Service, SourceNode } from "@openhps/core";
+import { Acceleration, Accelerometer, AngularVelocity, CallbackSinkNode, DataFrame, Gyroscope, LinearAccelerationSensor, Model, ModelBuilder, SensorObject, SensorValue, Service, SourceNode } from "@openhps/core";
 
 /**
- * IMU calibration service
+ * IMU calibration service using user interaction
+ * 
+ * ## Usage
+ * ```typscript
+ * 
+ * ```
  */
 export class IMUCalibrationService extends Service {
-    protected node: SourceNode;
-
-    constructor() {
+    protected options: IMUCalibrationOptions;
+    
+    constructor(options: IMUCalibrationOptions) {
         super();
-        this.on('build', this._onBuild.bind(this));
+        this.options = options;
     }
 
-    private _onBuild(): Promise<void> {
+    /**
+     * Calibrate the IMU sensor using user interaction
+     *
+     * @param {Function} userAction User action callback
+     * @returns 
+     */
+    calibrate(userAction: (step: IMUCalibrationStep) => Promise<void>): Promise<void> {
         return new Promise((resolve, reject) => {
+            // Calibration data
+            let running = true;
+            let model: Model;
+            let accelerometer: Accelerometer | LinearAccelerationSensor;
+            let gyroscope: Gyroscope;
+            let accelerationData: Acceleration[] = [];
+            let gyroscopeData: AngularVelocity[] = [];
 
-        });
-    }
+            function complete() {
+                if (!running) {
+                    return;
+                }
+                running = false;
 
-    calibrate(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            this.node.pull()
+                // Compute results per sensor
+                data.forEach((values, sensor) => {
+
+                });
+
+                // Finalize
+                if (model) {
+                    model.emitAsync('destroy')
+                        .then(() => resolve()).catch(reject);
+                } else {
+                    resolve();
+                }
+            }
+
+            userAction(IMUCalibrationStep.UPWARD).then(() => {
+                ModelBuilder.create()
+                    .from(this.options.source)
+                    .to(new CallbackSinkNode((frame: DataFrame) => {
+                        accelerometer = frame.getSensor(LinearAccelerationSensor) ?? frame.getSensor(Accelerometer);
+                        gyroscope = frame.getSensor(Gyroscope);
+                        accelerationData.push(accelerometer.value);
+                        gyroscopeData.push(gyroscope.value);
+                    }))
+                    .build().then((m: Model) => {
+                        model = m;
+                    }).catch(reject);
+            });
         });
     }
 }
 
 export interface IMUCalibrationOptions {
+    source: SourceNode;
+    minData: number;
+}
 
+export enum IMUCalibrationStep {
+    /**
+     * Position the IMU sensor upwards
+     */
+    UPWARD,
+    /**
+     * Position the IMU sensor downwards
+     */
+    DOWNWARD,
+    /**
+     * Position the IMU sensor perpendicular
+     */
+    PERPINDICULAR
 }
